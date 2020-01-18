3bc14a7e3914971e2a795b6cf31fba76b1b96af8
Chaichana <49665324+sun-23@users.noreply.github.com>
2020-01-18 20:18:22 +0700

    PlaySound

---
+++ PlaySound/PlaySound/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace PlaySound
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer s = new SoundPlayer(@"Sound\CoinWav.wav");
+            s.Play();
+        }
+    }
+}
