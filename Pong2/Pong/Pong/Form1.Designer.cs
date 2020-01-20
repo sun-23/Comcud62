411cd8a6ded9f63679470670b639aa3771c8c1f0
Chaichana <49665324+sun-23@users.noreply.github.com>
2020-01-20 10:21:52 +0700

    ok

---
+++ Pong2/Pong/Pong/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.paddle1 = new System.Windows.Forms.PictureBox();
+            this.paddle2 = new System.Windows.Forms.PictureBox();
+            this.aTimerPaddle = new System.Windows.Forms.Timer(this.components);
+            this.aBall = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.P1Label = new System.Windows.Forms.Label();
+            this.P2Label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paddle1
+            // 
+            this.paddle1.BackColor = System.Drawing.Color.White;
+            this.paddle1.Location = new System.Drawing.Point(16, 215);
+            this.paddle1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.paddle1.Name = "paddle1";
+            this.paddle1.Size = new System.Drawing.Size(13, 123);
+            this.paddle1.TabIndex = 0;
+            this.paddle1.TabStop = false;
+            // 
+            // paddle2
+            // 
+            this.paddle2.BackColor = System.Drawing.Color.White;
+            this.paddle2.Location = new System.Drawing.Point(1037, 215);
+            this.paddle2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.paddle2.Name = "paddle2";
+            this.paddle2.Size = new System.Drawing.Size(13, 123);
+            this.paddle2.TabIndex = 1;
+            this.paddle2.TabStop = false;
+            // 
+            // aTimerPaddle
+            // 
+            this.aTimerPaddle.Enabled = true;
+            this.aTimerPaddle.Tick += new System.EventHandler(this.aTimerPaddle_Tick);
+            // 
+            // aBall
+            // 
+            this.aBall.BackColor = System.Drawing.Color.White;
+            this.aBall.Location = new System.Drawing.Point(519, 265);
+            this.aBall.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.aBall.Name = "aBall";
+            this.aBall.Size = new System.Drawing.Size(13, 12);
+            this.aBall.TabIndex = 2;
+            this.aBall.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(295, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Player 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(613, 27);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Player 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(508, 65);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 58);
+            this.label3.TabIndex = 5;
+            this.label3.Text = ":";
+            // 
+            // P1Label
+            // 
+            this.P1Label.AutoSize = true;
+            this.P1Label.BackColor = System.Drawing.Color.Transparent;
+            this.P1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.P1Label.Location = new System.Drawing.Point(343, 102);
+            this.P1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.P1Label.Name = "P1Label";
+            this.P1Label.Size = new System.Drawing.Size(29, 31);
+            this.P1Label.TabIndex = 6;
+            this.P1Label.Text = "0";
+            // 
+            // P2Label
+            // 
+            this.P2Label.AutoSize = true;
+            this.P2Label.BackColor = System.Drawing.Color.Transparent;
+            this.P2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.P2Label.Location = new System.Drawing.Point(660, 102);
+            this.P2Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.P2Label.Name = "P2Label";
+            this.P2Label.Size = new System.Drawing.Size(29, 31);
+            this.P2Label.TabIndex = 7;
+            this.P2Label.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.P2Label);
+            this.Controls.Add(this.P1Label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.aBall);
+            this.Controls.Add(this.paddle2);
+            this.Controls.Add(this.paddle1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.paddle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox paddle1;
+        private System.Windows.Forms.PictureBox paddle2;
+        public System.Windows.Forms.PictureBox aBall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label P1Label;
+        private System.Windows.Forms.Label P2Label;
+        public System.Windows.Forms.Timer aTimerPaddle;
+    }
+}
+
---
+++ Pong2/Pong/Pong/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media; // for sound
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+
+        bool isP1Up, isP1Down, isP2Up, isP2Down; //updown
+        bool isBallLeft, isBallRight, isBallUp, isBallDown, isBallOut; // ball
+        int p1score = 0, p2score = 0; // score
+        int level = 0; //speed ball
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Timer
+        private void aTimerPaddle_Tick(object sender, EventArgs e)
+        {
+            DoMove(isP1Up, isP1Down, paddle1);//move paddle1
+            DoMove(isP2Up, isP2Down, paddle2);//move paddle2
+            CheckMoveBall(aBall, paddle1, paddle2); //Check Move Ball
+            MoveBall(aBall,isBallLeft,isBallRight,isBallOut,isBallDown,isBallUp); //move ball
+            CheckScore();
+        }
+
+        //Domove
+        void DoMove(bool isUp,bool isDown, PictureBox paddle)
+        {
+            int position_y = 0;
+
+            if (isUp)
+            {
+                position_y = -20;//move up
+            }
+            else if(isDown)
+            {
+                position_y = 20;//move down
+            }
+                                                                   // topFrom, BottomFrom
+            paddle.Location = new Point(paddle.Location.X, Math.Max(0,Math.Min(Height-140,paddle.Location.Y + position_y)));// set paddle position
+        }
+
+        //MoveBall
+        void MoveBall(PictureBox Ball, bool isLeft, bool isRight, bool isOut, bool isDown, bool isUp)
+        {
+            int positionBallX = 0;
+            int positionBallY = 0;
+
+            if (isLeft)
+            {
+                positionBallX = -(10+level);//ball go left
+            }
+            else if (isRight)
+            {
+                positionBallX = 10+level; //ball go right
+            }
+            else if (isOut)
+            {
+                //set ball center
+                positionBallX = Width/2 - Ball.Location.X;
+                positionBallY = Height / 2 - Ball.Location.Y;
+                // reset speed ball
+                level = 0;
+                // runball again
+                bool myRandomBoolUp = new Random().Next(100) <= 50 ? true : false;
+                if (myRandomBoolUp)
+                {
+                    isBallLeft = true;
+                }
+                else
+                {
+                    isBallRight = true;
+                }
+                //isBallDown = false;
+                //isBallUp = false;
+                
+            }
+
+            if (isBallUp)
+            {
+                positionBallY = -10; //ball go up
+            }
+            else if (isBallDown)
+            {
+                positionBallY = 10; //ball go down
+            }
+
+            Ball.Location = new Point(Ball.Location.X + positionBallX, Ball.Location.Y + positionBallY);
+        }
+
+        //CheckMoveBall
+        void CheckMoveBall(PictureBox Ball,PictureBox Paddle1, PictureBox Paddle2)
+        {
+            if (Ball.Location.X >= Paddle2.Location.X - Paddle2.Width && Ball.Location.Y >= Paddle2.Location.Y && Ball.Location.Y <= Paddle2.Location.Y + Paddle2.Height)
+            {
+                //ball touch paddle 2 then go left
+                isBallRight = false;
+                isBallLeft = true;
+                isBallOut = false;
+                level += 5;
+                RandomYAxis();
+            }
+            else if (Ball.Location.X <= Paddle1.Location.X + Paddle1.Width && Ball.Location.Y >= Paddle1.Location.Y && Ball.Location.Y <= Paddle1.Location.Y + Paddle1.Height)
+            {
+                //ball touch paddle 1 then go right
+                isBallLeft = false;
+                isBallRight = true;
+                isBallOut = false;
+                level += 5;
+                RandomYAxis();
+            }
+            else if (Ball.Location.X <= Paddle1.Location.X - Paddle1.Width && Ball.Location.Y <= Paddle1.Location.Y || Ball.Location.X <= Paddle1.Location.X - Paddle1.Width && Ball.Location.Y >= Paddle1.Location.Y + Paddle1.Height)
+            {
+                //ball out of paddle 1
+                isBallLeft = false;
+                isBallRight = false;
+                isBallOut = true;
+                // p2 + 1
+                Score(false);
+                PlaySound("GameOver.wav");
+                PlaySound("8bit.wav");
+            }
+            else if (Ball.Location.X >= Paddle2.Location.X + Paddle2.Width && Ball.Location.Y <= Paddle2.Location.Y || Ball.Location.X >= Paddle2.Location.X + Paddle2.Width && Ball.Location.Y >= Paddle2.Location.Y + Paddle2.Height)
+            {
+                //ball out of paddle 2
+                isBallLeft = false;
+                isBallRight = false;
+                isBallOut = true;
+                // p1 + 1
+                Score(true);
+                PlaySound("GameOver.wav");
+                PlaySound("8bit.wav");
+            }
+            else if(Ball.Location.Y <= 0)
+            {
+                //Ball postitionY is 0 then Ball go Down
+                isBallDown = true;
+                isBallUp = false;
+            }
+            else if(Ball.Location.Y >= Height-Ball.Height*5)
+            {
+                //Ball postitionY is From Height then Ball go Up
+                isBallDown = false;
+                isBallUp = true;
+            }
+        }
+
+        //PlaySound
+        void PlaySound(string name)
+        {
+            SoundPlayer s = new SoundPlayer(@"Sound\"+name); //new sound file Sound\name.wav
+            s.Play();
+        }
+
+        //CheckScore
+        void CheckScore()
+        {
+            if (p1score == 2)
+            {
+                Reset();
+                aTimerPaddle.Enabled = false; //stop timer
+                Form2 f2 = new Form2();
+                f2.Show(); // show form2
+                MessageBox.Show("P1 WIN!!!!");
+            }
+            else if (p2score == 2)
+            {
+                Reset();
+                aTimerPaddle.Enabled = false;//stop timer
+                Form2 f2 = new Form2();
+                f2.Show(); // show form2
+                MessageBox.Show("P2 WIN!!!!");
+            }
+        }
+
+        //Reset
+        void Reset()
+        {
+            level = 0;
+            p1score = 0;
+            p2score = 0;
+            P1Label.Text = p1score.ToString(); //setlabel
+            P2Label.Text = p2score.ToString(); //setlabel
+        }
+
+        //Score
+        void Score(bool isP1NotOut)
+        {
+            if (isP1NotOut)
+            {
+                p1score += 1;
+                P1Label.Text = p1score.ToString(); //setlabel
+            }
+            else
+            {
+                p2score += 1;
+                P2Label.Text = p2score.ToString(); //setlabel
+            }
+        }
+
+        //Random
+        void RandomYAxis()
+        {
+            bool myRandomBoolUp = new Random().Next(100) <= 50 ? true : false;
+            if (myRandomBoolUp)
+            {
+                isBallDown = false;
+                isBallUp = true;
+            }
+            else
+            {
+                isBallDown = true;
+                isBallUp = false;
+            }
+        }
+
+        // form load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            isBallRight = true;
+            PlaySound("8bit.wav");
+        }
+
+        // KeyDown
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            CheckKeys(e, true);
+        }
+
+        // KeyUp
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            CheckKeys(e, false);
+        }
+
+        //ChackKeys
+        void CheckKeys(KeyEventArgs e, bool isPresse)
+        {
+            switch(e.KeyCode) {
+                case Keys.W:
+                    isP1Up = isPresse;
+                    break;
+                case Keys.S:
+                    isP1Down = isPresse;
+                break;
+                case Keys.Up:
+                    isP2Up = isPresse;
+                break;
+                case Keys.Down:
+                    isP2Down = isPresse;
+                break;
+            }
+        }
+    }
+}
