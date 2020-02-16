b3b95ee60e0dc88655ebd62feb9abf7e58615fc4
Chaichana <49665324+sun-23@users.noreply.github.com>
2020-02-16 16:58:41 +0700

    Add MediaPlayer
    Media Player

---
+++ MediaPlayer/MediaPlayer/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string textUrl;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open1 = new OpenFileDialog();
+            if(open1.ShowDialog() == System.Windows.Forms.DialogResult.OK){
+                textUrl = open1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = textUrl;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+    }
+}
